1467eb4c37ce0dd73cfa1bd17d76ceaf13d8b32d
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-21 14:55:57 +0600

    button click add

--- src/Compontent/Bottle/Bottle.jsx
+++ src/Compontent/Bottle/Bottle.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import './Bottle.css'
 const Bottle = ({bottle}) => {
     const {name, img, price} =bottle
-    console.log(bottle)
+    // console.log(bottle);
+    const [data,setData] =useState(false);
+    const hendleButton =() =>{
+        setData(!data)
+        console.log(data)
+    }
     return (
         <div className="bottle">
         <h3>Bottle {name}</h3>    
         <img src={img} alt="" />
-        <p>Price: {price}</p>
+        <button onClick={hendleButton} className='btn-price'> {data?"Price : "+price: "Add"}</button>
         </div>
     );
 };
