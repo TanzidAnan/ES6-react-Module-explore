33eafe12fddf2bead9b04ebe1f5fc8216350e965
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-21 11:15:31 +0600

    calcutater js

--- src/Compontent/Sungles/Sungless.jsx
+++ src/Compontent/Sungles/Sungless.jsx
@@ -1,10 +1,13 @@
-import add from "../../Utiles/Calculate";
+import { add, multy } from "../../Utiles/Calculate";
+
 
 const Sungless = () => {
 
     const fast = 50;
     const second =120;
     const sum = add(fast,second);
+    const multys =multy(fast,second);
+    console.log(multys)
     console.log(sum)
 
     return (
