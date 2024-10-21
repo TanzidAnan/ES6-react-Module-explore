bae621dc288b86712edb27bb2da9f5fab5cdfda2
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-21 15:50:30 +0600

    cart data

--- src/Compontent/Bottle/Bottle.jsx
+++ src/Compontent/Bottle/Bottle.jsx
@@ -1,18 +1,22 @@
 import { useState } from 'react';
 import './Bottle.css'
-const Bottle = ({ bottle }) => {
+const Bottle = ({ bottle,hendleAddTocard }) => {
     const { name, img, price } = bottle;
     // console.log(bottle);
     const [data, setData] = useState(false);
     const hendleButton = () => {
         setData(!data);
-        console.log(data);
+
     }
+    
     return (
         <div className="bottle">
             <h3>Bottle {name}</h3>
             <img src={img} alt="" />
+            <div style={{display:'flex', gap:'15px'}}>
             <button onClick={hendleButton} className='btn-price'> {!data ? "Price : " + price : "Add"}</button>
+            <button onClick={() =>hendleAddTocard(bottle)} className='btn-Add'>Purchase</button>
+            </div>
         </div>
     );
 };
--- src/Compontent/Bottles/Bottles.jsx
+++ src/Compontent/Bottles/Bottles.jsx
@@ -5,17 +5,29 @@ import './Bottles.css'
 
 const Bottles = () => {
     const [bottles, setBottles] =useState([]);
+    const [cart,setCart] =useState([])
     useEffect(() =>{
         fetch('water.json')
         .then(res =>res.json())
         .then(data => setBottles(data))
     },[])
+
+    const hendleAddTocard =(bottle) =>{
+        const newCart =[...cart,bottle];
+        setCart(newCart)
+    }
+
     return (
         <div className="bottle-container">
             <h1>Bottles {bottles.length}</h1>
+            <h6>Cart: {cart.length}</h6>
             <div className="bottles">
             {
-                bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                bottles.map(bottle => <Bottle
+                     key={bottle.id}
+                      bottle={bottle}
+                      hendleAddTocard={hendleAddTocard}
+                      ></Bottle>)
             }
             </div>
         </div>
