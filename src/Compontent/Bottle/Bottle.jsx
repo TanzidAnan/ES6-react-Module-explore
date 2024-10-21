4788d85ba841f08fc6394286d5dc49dc3854c141
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-21 12:41:26 +0600

    data load

---
+++ src/Compontent/Bottle/Bottle.jsx
@@ -0,0 +1,11 @@
+
+const Bottle = ({bottle}) => {
+    console.log(bottle)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Bottle;
--- src/Compontent/Bottles/Bottles.jsx
+++ src/Compontent/Bottles/Bottles.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Bottle from "../Bottle/Bottle";
 
 const Bottles = () => {
     const [bottles, setBottles] =useState([]);
@@ -11,6 +12,9 @@ const Bottles = () => {
     return (
         <div>
             <h1>Bottles {bottles.length}</h1>
+            {
+                bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+            }
         </div>
     );
 };
